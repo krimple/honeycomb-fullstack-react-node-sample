c043ae64d6377a741aca81d62fc7a63c394117d4
Ken Rimple <kenrimple@honeycomb.io>
2024-11-19 08:10:08 -0500

    removed comment

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,9 +4,6 @@ import App from './App'
 import Observability from "./Observability";
 import {StrictMode} from "react";
 
-console.log(`Reloading ${new Date().toString()}`);
-// note: removed StrictMode to avoid double-rendering in dev
-// for the otel demo
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <Observability />
