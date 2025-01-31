d0a847e9398178137e0a266796d4cf05526bafe4
Ken Rimple <kenrimple@honeycomb.io>
2025-01-31 15:58:17 -0500

    waaat?

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -9,27 +9,43 @@ import react from "@vitejs/plugin-react";
 dotenv.config({
   path: path.resolve(
     __dirname,
-    process.env.NODE_ENV ? "." + process.env.NODE_ENV : "",
+    process.env.NODE_ENV ? "." + process.env.NODE_ENV : ""
   ),
 });
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-   sourcemap: 'hidden',
-  },
-  plugins: [react(), bundlesize()],
-  server: {
-    // https: {
-    //   key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
-    //   cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
-    // },
-    proxy: {
-      "/api": {
-        target: process.env.VITE_OTEL_EXPORTER_OTLP_ENDPOINT,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
+export default defineConfig(({ mode }) => {
+  // externalize sourcemaps (hidden) for production, keep inline for dev
+  console.log(mode);
+  const sourcemap = mode === "production" ? "hidden" : "inline";
+
+  const plugins =
+    mode === "production"
+      ? [react()]
+      : [
+          react(),
+          bundlesize({
+            stats: "all",
+          }),
+        ];
+
+  return {
+    build: {
+      sourcemap: sourcemap
+    },
+    plugins: plugins,
+    server: {
+      // https: {
+      //   key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
+      //   cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
+      // },
+      proxy: {
+        "/api": {
+          target: process.env.VITE_OTEL_EXPORTER_OTLP_ENDPOINT,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api"),
+        },
       },
     },
-  },
+  };
 });
