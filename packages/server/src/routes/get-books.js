4e6bc2166de5a2e4b5c1254f5ef015ad9a89fc51
Ken Rimple <kenrimple@honeycomb.io>
2025-02-09 10:30:54 -0500

    wip

--- packages/server/src/routes/get-books.js
+++ packages/server/src/routes/get-books.js
@@ -3,18 +3,27 @@ const logger = require("../logger");
 
 
 const getBooks = async (req, res) => {
-   try {
-       const pool = getPool();
-       // renaming to camelCase for returned data, ibid
-        const result = await pool.query(
-            `SELECT isbn, name, description, publication_date AS "publicationDate"
+    return new Promise(async (resolve) => {
+        try {
+            const pool = getPool();
+            // renaming to camelCase for returned data, ibid
+            const result = await pool.query(
+                `SELECT isbn, name, description, publication_date AS "publicationDate"
              FROM books 
              ORDER BY id ASC`);
-        res.json(result.rows);
-    } catch (err) {
-        logger.error(err);
-        res.status(500);
-    }
+            // every 3rd request slow waaaay down!
+            setTimeout(() => {
+                res.json(result.rows);
+            }, 8500);
+        } catch (err) {
+            reject();
+            logger.error(err);
+            res.status(500);
+            return;
+        }
+        resolve();
+    });
+
 };
 
 module.exports = { getBooks };
